bcbb905c0ee5488264920ff2cbfc64776788d615
MasterOutLite <schurik458@gmail.com>
2024-11-14 11:57:32 +0200

    tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind({ applyBaseStyles: false, configFile: "./tailwind.config.mjs" }),
+  ],
+  output: "server",
+  build: { inlineStylesheets: "never" },
+  // trailingSlash: "never",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "ua"],
+  },
+});
---
+++ tailwind.home.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/pages/index.astro", "./src/pages/[lang]/index.astro"],
+  theme: {
+    extend: {},
+  },
+};
