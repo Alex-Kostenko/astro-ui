2302adfd03f1a3b4f68563ed5e182653ed262728
MasterOutLite <schurik458@gmail.com>
2024-11-14 15:23:37 +0200

    configs alias and tailwind

--- tailwind.home.config.mjs
+++ tailwind.home.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const customConfig = require("./tailwind.config");
 export default {
-  content: ["./src/pages/index.astro", "./src/pages/[lang]/index.astro"],
+  content: [
+    "./src/pages/index.astro",
+    "./src/pages/[lang]/index.astro",
+    ...customConfig.content,
+  ],
   theme: {
-    extend: {},
+    extend: {
+      ...customConfig.theme.extend,
+    },
   },
+  plugins: [...customConfig.plugins],
 };
