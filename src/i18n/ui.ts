5ec07a3216640a8a2b3a6b7ee3f2bca2aa0597e8
MasterOutLite <schurik458@gmail.com>
2024-11-14 11:57:43 +0200

    i18n config

---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const languages = {
+  en: "English",
+  ua: "Ukrain",
+};
+
+export const defaultLang = "en";
+
+export const ui: any = {
+  en: {
+    home: "Home",
+    nav: {
+      home: "Home",
+    },
+  },
+  ua: {
+    home: "Головна",
+    nav: {
+      home: "Головна",
+    },
+  },
+} as const;
---
+++ src/i18n/utils.ts
@@ -0,0 +1,41 @@
+import { ui, defaultLang, languages } from "./ui";
+
+export function getLangFromUrl(url: URL) {
+  const [, lang] = url.pathname.split("/");
+  if (lang in ui) return lang as keyof typeof languages;
+  return defaultLang;
+}
+
+export function useTranslations(lang: keyof typeof languages, ns?: string) {
+  const getNestedTranslation = (obj: any, path: string): string | undefined => {
+    return path.split(".").reduce((acc, part) => acc && acc[part], obj);
+  };
+
+  if (ns) {
+    return function t(key: string) {
+      const translation =
+        getNestedTranslation(ui[lang]?.[ns], key) ||
+        getNestedTranslation(ui[defaultLang]?.[ns], key);
+
+      if (!translation) {
+        throw new Error(
+          `Translation for key "${key}" in namespace "${ns}" not found for language "${lang.toString()}".`,
+        );
+      }
+      return translation;
+    };
+  }
+
+  return function t(key: string) {
+    const translation =
+      getNestedTranslation(ui[lang], key) ||
+      getNestedTranslation(ui[defaultLang], key);
+
+    if (!translation) {
+      throw new Error(
+        `Translation for key "${key}" not found for language "${lang.toString()}".`,
+      );
+    }
+    return translation;
+  };
+}
