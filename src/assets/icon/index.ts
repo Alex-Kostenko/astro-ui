de33899c3f61633a1ef1e2937f5295d9c60efec6
MasterOutLite <schurik458@gmail.com>
2024-11-20 11:09:34 +0200

    comment card, fixed carousel size

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -2,6 +2,7 @@ import smallLogo from "./smallLogo.svg";
 import logo from "./logo.svg";
 import clutch from "./Clutch.svg";
 import linkedin from "./Linkedin.svg";
+import star from "./Star.svg";
 import * as company from "./company";
 
-export { smallLogo, logo, company, clutch, linkedin };
+export { smallLogo, logo, company, clutch, linkedin, star };
