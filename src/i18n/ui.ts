deb42db2d1b9f4a471e1bf3bf22271b77892d993
MasterOutLite <schurik458@gmail.com>
2024-11-21 13:44:07 +0200

    updated text config

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -22,7 +22,14 @@ export const ui: any = {
       about: {
         description:
           "We are firm believers that: no matter the size or budgets of a company, they should have access to the very best technical expertise to overcome any challenge. We also know that talented professionals crave dynamic and engaging environments to truly shine. Our mission is to unite these two values and empower businesses and professionals alike to achieve greatness.",
+        aboutUs: "more about us",
       },
+    },
+    services: {
+      tag: "SERVICES",
+      title:
+        "Our services will help you to accomplish your purpose easier and better.",
+      explore: "explore",
     },
   },
   ua: {
@@ -36,6 +43,7 @@ export const ui: any = {
       about: {
         description:
           "Ми твердо переконані, що: незалежно від розміру чи бюджету компанії, вони повинні мати доступ до найкращих технічних знань, щоб подолати будь-яку проблему. Ми також знаємо, що талановиті професіонали жадають динамічного та привабливого середовища, щоб справді сяяти. Наша місія полягає в тому, щоб об’єднати ці дві цінності та надати можливість як компаніям, так і професіоналам досягти величі.",
+        aboutUs: "more about us",
       },
     },
   },
