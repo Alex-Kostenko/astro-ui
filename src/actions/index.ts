266f230c5ec8446b38c0284b5b3ba939976b247a
MasterOutLite <schurik458@gmail.com>
2024-11-27 10:11:00 +0200

    server action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,3 +1,7 @@
+import { UploadPath } from "@interfaces/index";
+import contactUsService from "@service/contactUs.service";
+import fileService from "@service/file.service";
+import { file } from "astro/loaders";
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
 
@@ -11,16 +15,31 @@ export const server = {
       message: z.string(),
       file: z
         .custom<File>((v) => v instanceof File)
-        .refine((file) => {
-          console.log(file);
-          return file.size > 0 && file.size < MAX_FILE_SIZE;
-        }),
+        .refine((file) => file.size > 0 && file.size < MAX_FILE_SIZE),
     }),
     accept: "form",
-    handler(input, context) {
-      console.log(input);
+    async handler(input, context) {
+      console.log("input: ", input);
 
-      return { success: true };
+      const file = await fileService.upload({
+        files: input.file,
+        path: UploadPath.document,
+      });
+
+      try {
+        await contactUsService.create({
+          email: input.email,
+          message: input.message,
+          name: input.name,
+          file: file[0].id.toString(),
+        });
+
+        return { success: true };
+      } catch (err) {
+        await fileService.delete(file[0].id.toString());
+      }
+
+      return { success: false };
     },
   }),
 };
