64275ffd8773938dbeb3ec3f3386bf728a4ba18a
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2024-12-11 14:36:39 +0200

    add tabs component

--- src/components/Sections/Service/index.ts
+++ src/components/Sections/Service/index.ts
@@ -2,5 +2,12 @@ import CasesSection from "./CasesSection.astro";
 import DescriptionSection from "./DescriptionSection.astro";
 import AboutServiceSection from "./AboutServiceSection.astro";
 import StepsSection from "./StepsSection.astro";
+import HeroTabsSection from "./HeroTabsSection.astro";
 
-export { CasesSection, DescriptionSection, AboutServiceSection, StepsSection };
+export {
+  CasesSection,
+  DescriptionSection,
+  AboutServiceSection,
+  StepsSection,
+  HeroTabsSection,
+};
