88792ceb5b2c4d56d24454b9e0157140ee68d7ff
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2024-12-11 11:13:11 +0200

    add service page

---
+++ src/components/Sections/Service/index.ts
@@ -0,0 +1,6 @@
+import CasesSection from "./CasesSection.astro";
+import DescriptionSection from "./DescriptionSection.astro";
+import AboutServiceSection from "./AboutServiceSection.astro";
+import StepsSection from "./StepsSection.astro";
+
+export { CasesSection, DescriptionSection, AboutServiceSection, StepsSection };
