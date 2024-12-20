417af05b568ed22ec90d4252d9cdfff1747da0e4
Vasyl Kharchenko <vkharchenko@ksf.kiev.ua>
2024-12-20 13:35:04 +0200

    saved update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "hybrid",
-  adapter: vercelServerless(),
+  output: "static",
+  // adapter: vercelServerless(),
   build: { inlineStylesheets: "never" },
   i18n: {
     defaultLocale: "en",
