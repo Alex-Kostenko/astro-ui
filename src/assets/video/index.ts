04c8cbcbfaab7c742f4677bf0efdceacf4296dcb
Vasyl Kharchenko <vkharchenko@ksf.kiev.ua>
2025-01-03 10:05:45 +0200

    updated type loading

--- src/assets/video/index.ts
+++ src/assets/video/index.ts
@@ -1,4 +1,4 @@
 import cubeNear from "./cube_near.mp4";
-import cubeSide from "./408250_Cube Cgi 3d Changing_By_Finn_Moeller_Artlist_HD.mp4";
+import cubeSide from "./cube_side.mp4";
 
 export { cubeNear, cubeSide };
