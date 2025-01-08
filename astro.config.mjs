5a42e986832fb26d8a64fe17b953bd5380f391f9
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-01-08 10:35:56 +0200

    add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,18 @@ import vercelServerless from "@astrojs/vercel";
 
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "static",
-  adapter: vercelServerless(),
   build: { inlineStylesheets: "never" },
+
   i18n: {
     defaultLocale: "en",
     locales: ["en", "ua"],
   },
+
+  adapter: cloudflare(),
 });
