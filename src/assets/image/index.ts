c4fd076ec79d85a9ef996d4a09b1f9434383ed3a
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-01-10 10:41:53 +0200

    compress image

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -1,6 +1,6 @@
-import Human from "./c5d7e0c2cbaf98777ad49fdeac0f44d5.png";
-import Circly from "./2d5e1d21006da59974413e512f55ba7e.png";
-import Eye from "./885eb6a27d799518a0f56dee300bf4c6.png";
+import Human from "./c5d7e0c2cbaf98777ad49fdeac0f44d5.webp";
+import Circly from "./2d5e1d21006da59974413e512f55ba7e.webp";
+import Eye from "./885eb6a27d799518a0f56dee300bf4c6.webp";
 import AboutService from "./aboutSerivce.webp";
 
 export { Human, Circly, Eye, AboutService };
