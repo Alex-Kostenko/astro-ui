37d06ded6e030561e3a8c0a869542bf9e25f8c73
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-01-13 16:17:33 +0200

    add fallback video with vp9 codec

--- src/assets/video/index.ts
+++ src/assets/video/index.ts
@@ -1,4 +1,6 @@
 import cubeNear from "./cube_near_reversed.mp4";
+import cubeNearVP9 from "./cube_near_reversed_vp9.mp4";
 import cubeSide from "./cube_side.mp4";
+import cubeSideVP9 from "./cube_side_vp9.mp4";
 
-export { cubeNear, cubeSide };
+export { cubeNear, cubeNearVP9, cubeSide, cubeSideVP9 };
