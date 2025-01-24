b3270ca7c78ffb43d93984dc2d7bbdb3e96b3db9
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-01-24 15:36:25 +0200

    change service steps icons

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -2,5 +2,6 @@ import Human from "./c5d7e0c2cbaf98777ad49fdeac0f44d5.webp";
 import Circly from "./2d5e1d21006da59974413e512f55ba7e.webp";
 import Eye from "./885eb6a27d799518a0f56dee300bf4c6.webp";
 import AboutService from "./aboutSerivce.webp";
+import { steps } from "./steps";
 
-export { Human, Circly, Eye, AboutService };
+export { Human, Circly, Eye, AboutService, steps };
---
+++ src/assets/image/steps/index.ts
@@ -0,0 +1,8 @@
+import step_1 from "./step_1.svg";
+import step_2 from "./step_2.svg";
+import step_3 from "./step_3.svg";
+import step_4 from "./step_4.svg";
+import step_5 from "./step_5.svg";
+import step_6 from "./step_6.svg";
+
+export const steps = [step_1, step_2, step_3, step_4, step_5, step_6];
