7e068e0c6389924f714e6d02b867d9ccff18d76b
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-01-30 17:12:06 +0200

    move career components to another directory

--- src/components/Sections/Vacancy/index.ts
+++
@@ -1,5 +0,0 @@
-import Advantages from "./Advantages.astro";
-import VacancyTitle from "./VacancyTitle.astro";
-import Positions from "./Positions.astro";
-
-export { Advantages, VacancyTitle, Positions };
