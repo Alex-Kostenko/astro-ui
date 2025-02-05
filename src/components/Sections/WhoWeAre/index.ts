e6d86f02fba5e679ef4efaf3acd12b7ef460dba8
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-02-05 17:18:55 +0200

    add who we are page

---
+++ src/components/Sections/WhoWeAre/index.ts
@@ -0,0 +1,6 @@
+export { default as Title } from "./Title.astro";
+export { default as Mission } from "./Mission.astro";
+export { default as Values } from "./Values.astro";
+export { default as Story } from "./Story.astro";
+export { default as Partners } from "./Partners.astro";
+export { default as Services } from "./Services.astro";
