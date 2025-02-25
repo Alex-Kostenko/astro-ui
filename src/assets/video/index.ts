047060b7e1cec9308da9d36a3db88301ef85f36e
Vasyl Kharchenko <vkharchenko@ksf.kiev.ua>
2025-02-25 11:28:47 +0200

    feat: add video import

--- src/assets/video/index.ts
+++ src/assets/video/index.ts
@@ -2,5 +2,10 @@ import cubeNear from "./cube_near.mp4";
 import cubeNearVP9 from "./cube_near_vp9.mp4";
 import cubeSide from "./cube_side.mp4";
 import cubeSideVP9 from "./cube_side_vp9.mp4";
+export { default as curveWaves } from "./curve-waves.mp4";
+import { default as loopWavy } from "./loop-wavy.mp4";
+import { default as lotOfWavesBlack } from "./lot-of-waves-black.mp4";
+import { default as rareWavesBlack } from "./rare-waves-black.mp4";
+import { default as whiteWavesIdle } from "./white-waves-idle.mp4";
 
 export { cubeNear, cubeNearVP9, cubeSide, cubeSideVP9 };
