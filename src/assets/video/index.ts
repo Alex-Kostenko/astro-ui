38be9a54c6db16032c2d1abc8974857cdec5b49e
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-03-10 10:37:47 +0200

    add assets

--- src/assets/video/index.ts
+++ src/assets/video/index.ts
@@ -8,5 +8,5 @@ export { default as lotOfWavesBlack } from "./lot-of-waves-black.mp4";
 export { default as rareWavesBlack } from "./rare-waves-black.mp4";
 export { default as whiteWavesIdle } from "./white-waves-idle.mp4";
 export { default as twistedTunel } from "./twisted-tunel.mp4";
-
+export { default as abstractLoopBgWithLines } from "./abstract-loop-background-with-lines.mp4";
 export { cubeNear, cubeNearVP9, cubeSide, cubeSideVP9 };
