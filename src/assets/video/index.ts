bb1229a20370a344f230d920b702e97249a2ebec
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-03-12 12:08:52 +0200

    add video with 264 codec

--- src/assets/video/index.ts
+++ src/assets/video/index.ts
@@ -1,4 +1,5 @@
 import cubeNear from "./cube_near.mp4";
+import cubeNear264 from "./cube_near_264.mp4";
 import cubeNearVP9 from "./cube_near_vp9.mp4";
 import cubeSide from "./cube_side.mp4";
 import cubeSideVP9 from "./cube_side_vp9.mp4";
@@ -9,4 +10,4 @@ export { default as rareWavesBlack } from "./rare-waves-black.mp4";
 export { default as whiteWavesIdle } from "./white-waves-idle.mp4";
 export { default as twistedTunel } from "./twisted-tunel.mp4";
 export { default as abstractLoopBgWithLines } from "./abstract-loop-background-with-lines.mp4";
-export { cubeNear, cubeNearVP9, cubeSide, cubeSideVP9 };
+export { cubeNear, cubeNearVP9, cubeNear264, cubeSide, cubeSideVP9 };
