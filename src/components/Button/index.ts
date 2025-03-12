e2c3762a3d042c149f434e68b6a1d5376686ca90
Vasyl Kharchenko <vkharchenko@ksf.kiev.ua>
2025-03-12 12:03:30 +0200

    feat: new button standart

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,3 +1,5 @@
 import ButtonAstro from "./ButtonAstro.astro";
+import ButtonSetAstro from "./ButtonSetAstro.astro";
+export * from "./button.type";
 
-export { ButtonAstro };
+export { ButtonAstro, ButtonSetAstro as Button };
