30688b9326f7faab4b4c5c417960e4900fc619a2
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-03-12 14:47:30 +0200

    update button set component

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,5 +1,6 @@
 import ButtonAstro from "./ButtonAstro.astro";
 import ButtonSetAstro from "./ButtonSetAstro.astro";
+import ButtonIcon from "./ButtonIcon.astro";
 export * from "./button.type";
 
-export { ButtonAstro, ButtonSetAstro as Button };
+export { ButtonAstro, ButtonSetAstro as Button, ButtonIcon };
