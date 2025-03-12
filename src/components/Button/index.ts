d655dac852d38fa3672780d3f16b0daa757593bf
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-03-12 16:43:32 +0200

    add arrowed button

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,6 +1,7 @@
 import ButtonAstro from "./ButtonAstro.astro";
 import ButtonSetAstro from "./ButtonSetAstro.astro";
 import ButtonIcon from "./ButtonIcon.astro";
+import ArrowedButton from "./ArrowedButton.astro";
 export * from "./button.type";
 
-export { ButtonAstro, ButtonSetAstro as Button, ButtonIcon };
+export { ButtonAstro, ButtonSetAstro as Button, ButtonIcon, ArrowedButton };
